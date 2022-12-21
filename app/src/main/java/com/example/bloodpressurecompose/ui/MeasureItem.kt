4800461222da24ca7a70853efc29932c3c8ad8b9
Kirill.Kupershmidt <kirill.kupershmidt@gmail.com>
2022-12-21 16:31:40 +0300

    animated cards

--- app/src/main/java/com/example/bloodpressurecompose/ui/MeasureItem.kt
+++ app/src/main/java/com/example/bloodpressurecompose/ui/MeasureItem.kt
@@ -1,23 +1,37 @@
 package com.example.bloodpressurecompose.ui
 
+import android.annotation.SuppressLint
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.bloodpressurecompose.R
 import com.example.bloodpressurecompose.model.Measurement
+import com.example.bloodpressurecompose.ui.theme.DayColor
+import com.example.bloodpressurecompose.ui.theme.NightColor
 import com.example.bloodpressurecompose.ui.theme.Shapes
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import java.text.DateFormat
+import kotlin.math.absoluteValue
 
 @Composable
 fun MeasureValue(@StringRes label: Int, value: Int?) {
@@ -39,54 +53,119 @@ fun MeasureValue(@StringRes label: Int, value: Int?) {
 
 }
 
+@SuppressLint("ReturnFromAwaitPointerEventScope", "MultipleAwaitPointerEventScopes")
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MeasureItem(
     modifier: Modifier = Modifier,
     measurement: Measurement,
     onClick: () -> Unit,
     onDeleteClick: () -> Unit) {
-    Card(
+
+    val offsetX = remember { Animatable(0f) }
+
+    Box(
+        contentAlignment = Alignment.CenterStart,
+
         modifier = modifier
-            .padding(horizontal = 8.dp, vertical = 4.dp)
-            .fillMaxWidth()
-            .clickable(true, onClick = onClick),
-        shape = Shapes.medium,
-        elevation = 4.dp
-    ) {
-        Column(modifier = modifier.padding(4.dp)) {
-            Text(
-                text = DateFormat.getDateInstance().format(measurement.date),
-                modifier = Modifier
-                    .fillMaxWidth(),
-                textAlign = TextAlign.End,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colors.primaryVariant
+        .padding(horizontal = 8.dp, vertical = 4.dp)
+        .fillMaxWidth()
+
+        .pointerInput(Unit) {
+            val offset = size.width.toFloat() / 3
+            offsetX.updateBounds(
+                lowerBound = -offset,
+                upperBound = 0f
             )
 
-            Row {
-                Column {
-                    Row(modifier.padding(4.dp)) {
-                        SunIcon(modifier.padding(start = 4.dp, end = 16.dp))
-                        MeasureValue(label = R.string.sys, value = measurement.morningSYS)
-                        MeasureValue(label = R.string.dia, value = measurement.morningDIA)
-                        MeasureValue(label = R.string.pulse, value = measurement.morningPulse)
+            coroutineScope {
+                while (true) {
+                    val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                    offsetX.stop()
+
+                    awaitPointerEventScope {
+                        horizontalDrag(pointerId) { change ->
+                            launch {
+                                offsetX.snapTo(offsetX.value + change.positionChange().x)
+                            }
+                        }
                     }
-                    Row(modifier.padding(4.dp)) {
-                        NightIcon(modifier.padding(start = 4.dp, end = 16.dp))
-                        MeasureValue(label = R.string.sys, value = measurement.eveningSYS)
-                        MeasureValue(label = R.string.dia, value = measurement.eveningDIA)
-                        MeasureValue(label = R.string.pulse, value = measurement.eveningPulse)
+
+                    launch {
+                        if(offsetX.value.absoluteValue <= offset / 2) {
+                            offsetX.animateTo(
+                                targetValue = offsetX.upperBound!!,
+                                animationSpec = tween(
+                                    easing = LinearEasing
+                                )
+                            )
+                        } else {
+                            offsetX.animateTo(
+                                targetValue = offsetX.lowerBound!!,
+                                animationSpec = tween(
+                                    easing = LinearEasing
+                                )
+                            )
+                        }
                     }
                 }
+            }
+        },
+    ) {
+        // background
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            IconButton(
+                onClick = onClick,
+                Modifier.padding(8.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "edit", tint = NightColor)
+            }
 
-                Spacer(modifier = Modifier.weight(1f))
+            IconButton(
+                onClick = onDeleteClick,
+                Modifier.padding(8.dp)
+            ) {
+                Icon(imageVector = Icons.Outlined.Delete, contentDescription = "delete", tint = DayColor)
+            }
+        }
 
-                /*
-                IconButton(onClick = onDeleteClick) {
-                    DeleteIcon(Modifier.fillMaxHeight().align(Alignment.Bottom))
+        // frontend
+        Card(
+            modifier = modifier
+                .offset { IntOffset(offsetX.value.toInt(), 0) },
+            shape = Shapes.medium,
+            elevation = 4.dp
+        ) {
+            Column(modifier = modifier.padding(4.dp)) {
+                Text(
+                    text = DateFormat.getDateInstance().format(measurement.date),
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.End,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.primaryVariant
+                )
+
+                Row {
+                    Column {
+                        Row(modifier.padding(4.dp)) {
+                            SunIcon(modifier.padding(start = 4.dp, end = 16.dp))
+                            MeasureValue(label = R.string.sys, value = measurement.morningSYS)
+                            MeasureValue(label = R.string.dia, value = measurement.morningDIA)
+                            MeasureValue(label = R.string.pulse, value = measurement.morningPulse)
+                        }
+                        Row(modifier.padding(4.dp)) {
+                            NightIcon(modifier.padding(start = 4.dp, end = 16.dp))
+                            MeasureValue(label = R.string.sys, value = measurement.eveningSYS)
+                            MeasureValue(label = R.string.dia, value = measurement.eveningDIA)
+                            MeasureValue(label = R.string.pulse, value = measurement.eveningPulse)
+                        }
+                    }
                 }
-
-                 */
             }
         }
     }
--- app/src/main/java/com/example/bloodpressurecompose/ui/MeasureList.kt
+++ app/src/main/java/com/example/bloodpressurecompose/ui/MeasureList.kt
@@ -1,8 +1,5 @@
 package com.example.bloodpressurecompose
 
-import android.graphics.drawable.Icon
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,19 +9,12 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.bloodpressurecompose.model.Measurement
-import com.example.bloodpressurecompose.ui.DeleteIcon
 import com.example.bloodpressurecompose.ui.MeasureItem
 import com.example.bloodpressurecompose.ui.viewmodel.MeasureViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import dagger.internal.DoubleCheck.lazy
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -54,37 +44,7 @@ fun MeasurementList(
     ) { padding ->
         LazyColumn {
             items(measureListItem, {measureListItem: Measurement -> measureListItem.id}) { item ->
-                val dismissState = rememberDismissState(
-                    confirmStateChange = { false }
-                )
-
-                SwipeToDismiss(
-                    state = dismissState,
-                    directions = setOf(DismissDirection.EndToStart),
-                    dismissThresholds = { FractionalThreshold(0.2f) },
-                    background = {
-                        val color by animateColorAsState (
-                            targetValue = when (dismissState.targetValue) {
-                                DismissValue.Default -> MaterialTheme.colors.background
-                                else -> Color.Red
-                            }
-                        )
-
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(4.dp)
-                                .background(color)
-                                .padding(horizontal = 12.dp),
-                            contentAlignment = Alignment.CenterEnd
-                        ) {
-                            IconButton(onClick = { viewModel.deleteMeasurement(item) }) {
-                                DeleteIcon()
-                            }
-                        }
-                    }
-                ) {
-                    MeasureItem (
+                 MeasureItem (
                         modifier = modifier.padding(padding),
                         measurement = item,
                         onClick = { editMeasureItem(item.id) },
@@ -93,7 +53,6 @@ fun MeasurementList(
                 }
             }
         }
-    }
 }
 
 
